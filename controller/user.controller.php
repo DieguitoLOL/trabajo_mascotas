0cfa1aec52d4291a0face872388f3523a367e1c4
DieguitoLOL <139809590+DieguitoLOL@users.noreply.github.com>
2023-10-26 15:41:15 -0500

    Agregando cambios

--- controller/user.controller.php
+++ controller/user.controller.php
@@ -1,4 +1,3 @@
-!
 <?php 
 require_once(__DIR__ ."/../model/User.model.php");
 require_once(__DIR__ ."conexiones.php");
@@ -6,10 +5,19 @@ class UserController extends Conexion {
     public function crear(User $user) {
        $mysqlA = $this->conectar();
        $nombre = $mysqlA -> real_escape_string($user->nombre);
+       $username = $mysqlA -> real_escape_string($user->username);
+       $email = $mysqlA -> real_escape_string($user->email);
+       $password = $mysqlA -> real_escape_string($user->password);
+       $foto = $mysqlA -> real_escape_string($user->foto);
 
-       $sql = "INSERT INTO User (nombre) VALUES ('$nombre')";
+       $sql = "INSERT INTO User (nombre) VALUES ('$nombre')"; 
+       $sql = "INSERT INTO User (username) VALUES ('$username')"; 
+       $sql = "INSERT INTO User (email) VALUES ('$email')"; 
+       $sql = "INSERT INTO User (password) VALUES ('$password')"; 
+       $sql = "INSERT INTO User (foto) VALUES ('$foto')"; 
+
+       return $this->conectar($sql);
        $mysqlA->close();
-       return $sql;
     }
 }
 ?>
--- model/User.model.php
+++ model/User.model.php
@@ -4,7 +4,7 @@ class user{
     public string $nombre;
     public string $username;
     public string $email;
-    private string $password;
+    public string $password;
     public int $Role_id;
     public string $foto;
 }
