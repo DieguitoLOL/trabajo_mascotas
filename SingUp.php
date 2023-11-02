532f8e00fc3427c3104887f19ce8e69b749a4be2
DieguitoLOL <139809590+DieguitoLOL@users.noreply.github.com>
2023-11-02 11:33:07 -0500

    Realizando diseño de la pagina

--- SingUp.php
+++ SingUp.php
@@ -12,68 +12,50 @@
         use Dotenv\Dotenv; 
         $dotenv = Dotenv::createImmutable(__DIR__);
         $dotenv->load();
-    ?>
+?>
 <main>
-    <section>
-        <div>
-            <div><img src="img/Logo.png" alt="Logo veterinaria"></div>
-            <h6>LifePet</h6>
+    <section class="seccion_ingreso">
+        <div class="contenido_logo">
+            <div><img class="logo" src="img/Logo.png" alt="Logo veterinaria"></div>
+            <h6 class="texto_logo">LifePet</h6>
         </div>
-        <div>
+        <div class="contenido_registro">
             <h1>Registro de Usuario</h1>
             <form  method="POST">    
                 <?php    
                     require_once __DIR__ ."/process/procesar_registro.php";
-                ?>   
-                <label for="Name">Nombre completo</label>
-                <input type="text" id="Name" name="Name" required value=""><br>
+                ?>  
+                <div class="registros">
+                  <label for="Name">Nombre completo</label><br>
+                  <input type="text" id="Name" name="Name" required value="">
+                </div>
 
-                <label for="UserName">Nombre de Usuario</label>
-                <input type="text" id="UserName" name="UserName" required value=""><br>
+                <div class="registros">
+                  <label for="UserName">Nombre de Usuario</label><br>
+                  <input type="text" id="UserName" name="UserName" required value="">
+                </div>
 
-                <label for="email">Correo Electrónico:</label>
-                <input type="email" id="email" name="email" required value=""><br>
+                <div class="registros">
+                  <label for="email">Correo Electrónico:</label><br>
+                  <input type="email" id="email" name="email" required value="">
+                </div>
 
-                <label for="contrasena">Contraseña:</label>
-                <input type="password" id="contrasena" name="contrasena" required><br>
-
+                <div class="registros">
+                  <label for="contrasena">Contraseña:</label><br>
+                  <input type="password" id="contrasena" name="contrasena" required>
+                </div class="registros"> 
                 <input name="btRegister" type="submit" value="Registrarse">
             </form>
         </div>
     </section>
-    <section>
-        <div><img src="img/ImagenGato.jpeg" alt="Imagen gatito"></div>
-        <div>
+    <section class="seccion_contenido">
+        <div class="seccion_contenido-img"><img class="imagen_contenido" src="img/ImagenGato.jpeg" alt="Imagen gatito"></div>
+        <div class="seccion_contenido-text">
             <h4>LifePet</h4>
             <p>En LifePet, hacemos que las sonrisas de tus mascotas sean nuestra especialidad. Tu confianza, su felicidad.</p>
         </div>
     </section>
 </main>
-
 </body>
 </html>
-<!-- <!DOCTYPE html>
-<html>
-<head>
-  <link rel="stylesheet" href="./css/style.css">
-  <title>Adopta un gatito en tu zona</title>
-</head>
-<body>
-  <div class="login-page">
-    <div class="form">
-      <div class="login">
-        <div class="login-header">
-          <h3>Adopta un gatito en tu zona</h3>
-          <p>Por favor, ingresa tus credenciales para iniciar sesión.</p>
-        </div>
-      </div>
-      <form class="login-form">
-        <input type="text" placeholder="Usuario o correo electrónico" name="uname" required>
-        <input type="password" placeholder="Contraseña" name="psw" required>
-        <button>Iniciar sesión</button>
-        <p class="message">¿No estás registrado? <a href="#">Crea una cuenta</a></p>
-      </form>
-    </div>
-  </div>
-</body>
-</html> -->
+
